e38a594f55b1a45e8a9da240cb5a3f7cdc424b82
Wesley De Witt <wesley.brian.dewit@gmail.com>
2025-03-28 14:11:44 -0400

    Added header menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "gatsby";
 import closeIcon from "../images/close-icon.svg";
+import Nav from "./Nav";
 import About from "./index-sections/About";
 
 const Menu = () => {
 	return (
 		<div className="menu" id="menu">
+			<Nav />
 			<About />
 		</div>
 	);
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,12 +4,29 @@ import { Link } from "gatsby";
 const Nav = () => {
 	return (
 		<nav className="nav">
-			<Link activeClassName="active" to="/">
-				Index
-			</Link>
-			<Link activeClassName="active" to="/feed">
-				Feed
-			</Link>
+			<div className="nav__content">
+				<Link activeClassName="active" to="/">
+					Index
+				</Link>
+				<Link activeClassName="active" to="/feed">
+					Feed
+				</Link>
+				<Link activeClassName="active" to="/feed">
+					Projects
+				</Link>
+				<Link activeClassName="active" to="/feed">
+					Essays
+				</Link>
+				<Link activeClassName="active" to="/feed">
+					Photos
+				</Link>
+				<Link activeClassName="active" to="/feed">
+					Travel
+				</Link>
+				<Link activeClassName="active" to="/feed">
+					Newsletter
+				</Link>
+			</div>
 		</nav>
 	);
 };
